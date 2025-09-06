d7234b934d9a24fbdaec9374aadca089e4a7656f
yoharnu <dani91@gmail.com>
2025-09-05 21:22:20 -0400

    Integrate Swagger/OpenAPI documentation support
    Updated Program.cs to configure Swagger services and UI for API documentation. Replaced Microsoft.AspNetCore.OpenApi package with Swashbuckle.AspNetCore for better integration.

--- api/Program.cs
+++ api/Program.cs
@@ -1,34 +1,60 @@
+using Microsoft.OpenApi.Models;
 
-namespace api
+namespace api;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Configure Swagger/OpenAPI
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            options.SwaggerDoc("v1", new OpenApiInfo
             {
-                app.MapOpenApi();
+                Title = "API Documentation",
+                Version = "v1",
+                Description = "This is the API documentation for the application.",
+                License = new OpenApiLicense
+                {
+                    Name = "GNU General Public License version 3",
+                    Url = new Uri("https://opensource.org/license/gpl-3-0")
+                }
+            });
+
+            // Add XML comments if available for better documentation
+            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            if (File.Exists(xmlPath))
+            {
+                options.IncludeXmlComments(xmlPath);
             }
+        });
 
-            app.UseHttpsRedirection();
+        var app = builder.Build();
 
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                options.RoutePrefix = "api/";
+            });
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
