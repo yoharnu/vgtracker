c7bacea00b7978d9d7163fccd721b05011631233
yoharnu <dani91@gmail.com>
2025-09-14 00:42:10 -0400

    Implement debouncing for game search functionality
    Refactor the `searchGame` function to use a debouncing technique, delaying execution until the user stops typing for 500 milliseconds. The original function has been removed, and a new `debounce` function has been added to manage the timing of API calls. The search will only trigger if the input length is 2 or more characters.

--- web/src/components/GameSearchBar.vue
+++ web/src/components/GameSearchBar.vue
@@ -4,23 +4,36 @@
   const searchbox = ref('')
   const dataArray = ref([])
 
-  async function searchGame() {
+  const searchGame = debounce(async function () {
     if (searchbox.value.length < 2) {
       return
     }
+
+    console.log('Searching for:', searchbox.value)
 
     const response = await fetch(`/api/IGDB/games/search/${encodeURIComponent(searchbox.value)}`)
     if (response.ok) {
       dataArray.value = await response.json()
     }
+  }, 500)
 
-    console.log('Searching for:', searchbox.value)
+  function debounce(fn, wait) {
+    let timer;
+    return function (...args) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      const context = this;
+      timer = setTimeout(() => {
+        fn.apply(context, args);
+      }, wait);
+    };
   }
 </script>
 
 <template>
   <input type="text" v-model="searchbox" @input="searchGame" />
-  <br/>
+  <br />
   <ul>
     <li v-for="data in dataArray">{{ data.name }}<span v-if="data.year > 0"> ({{data.year}})</span></li>
   </ul>
