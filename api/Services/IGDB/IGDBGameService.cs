6dee930c4f91860e41e1413cae5ac51e5488fa85
yoharnu <dani91@gmail.com>
2025-09-14 22:20:41 -0400

    Add cover image support to game search results
    - Updated `SearchResult` class to include a `Cover` property.
    - Modified constructor to set `Cover` from the `Game` object.
    - Enhanced game data query to retrieve cover image fields.
    - Updated `GameSearchResult.vue` to display the cover image with a new layout.
    - Added styling to limit cover image width to 90 pixels.

--- api/Services/IGDB/IGDBGameService.cs
+++ api/Services/IGDB/IGDBGameService.cs
@@ -12,6 +12,7 @@ public class SearchResult
     public string GameType { get; set; } = string.Empty;
     public string ParentName { get; set; } = string.Empty;
     public int ParentYear { get; set; }
+    public Cover? Cover { get; set; }
 
     [JsonConstructor]
     public SearchResult() { }
@@ -23,6 +24,9 @@ public class SearchResult
         this.GameType = game.GameType.Value.Type;
         this.ParentName = game.ParentGame?.Value.Name ?? string.Empty;
         this.ParentYear = game.ParentGame?.Value.FirstReleaseDate?.Year ?? 0;
+        this.Cover = game.Cover?.Value;
+        if (this.Cover != null)
+            this.Cover.Url = "https://images.igdb.com/igdb/image/upload/t_cover_small/" + this.Cover.ImageId + ".jpg";
     }
 }
 
@@ -42,7 +46,7 @@ public class IGDBGameService(IGDBClient client)
 
         int _limit = Math.Min(limit ?? 5, 100);
 
-        var searchResults = await client.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: $"search \"{name}\"; fields id,name,first_release_date,game_type.type,parent_game.name,parent_game.game_type,parent_game.name,parent_game.first_release_date; where game_type = (0,1,4,8,10,9,2) & version_parent = null; limit {_limit};");
+        var searchResults = await client.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: $"search \"{name}\"; fields id,name,first_release_date,game_type.type,parent_game.name,parent_game.game_type,parent_game.name,parent_game.first_release_date,cover.*; where game_type = (0,1,4,8,10,9,2) & version_parent = null; limit {_limit};");
         return searchResults
             .Select(x => new SearchResult(x))
             .ToList();
--- web/src/components/GameSearchResult.vue
+++ web/src/components/GameSearchResult.vue
@@ -5,9 +5,23 @@
 
 <template>
   <div class="card">
-    <div class="card-body">
-      <div class="card-title">{{ data.name }}<span v-if="data.year > 0"> ({{data.year}})</span></div>
-      <div class="card-text" v-if="data.parentName"> {{data.parentName}}<span v-if="data.parentYear > 0"> ({{data.parentYear}})</span> {{data.gameType}}</div>
+    <div class="card-body container">
+      <div class="row">
+        <div class="col-1">
+          <img v-if="data.cover.url" :src="data.cover.url" class="searchCover" />
+        </div>
+        <div class="col">
+          <h5 class="card-title">{{ data.name }}<span v-if="data.year > 0"> ({{data.year}})</span></h5>
+          <small class="card-text" v-if="data.parentName"> {{data.parentName}}<span v-if="data.parentYear > 0"> ({{data.parentYear}})</span> {{data.gameType}}</small>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+
+<style>
+  .searchCover {
+    max-width: 90px;
+  }
+</style>
